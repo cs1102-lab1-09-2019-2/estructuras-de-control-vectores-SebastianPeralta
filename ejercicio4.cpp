96c2f6226bffef3bf59551ffc772f38cb41eaaad
SebastianPeralta <54275767+SebastianPeralta@users.noreply.github.com>
2019-08-29 23:00:31 -0500

    Update ejercicio4.cpp

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
+#include <iomanip>
 using namespace std;
-int main() {
-    float a, b, c, delta, x1, x2;
-    cout<<"Ingrese los valores los 3 coeficientes de x de la ecuación cuadrática separados por espacio:\n";
-    cin>>a>>b>>c;
-    delta=(b*b-4*a*c);
-    if (a==0 and b==0) {
-        cout<<"No tiene solución\n";
-    } else if (a==0) {
-        cout<<"x = "<<-c/b<<"\n";
-    } else if (delta<0){
-        cout<<"No tiene solución\n";
-    } else {
-        x1=(-b+(sqrt(delta)))/(2*a);
-        x2=(-b-(sqrt(delta)))/(2*a);
-        cout<<"x1 = "<<x1<<", x2 = "<<x2<<"\n";
+int main(){ int num;
+    for (int i = 1; i <7 ; ++i) {
+        for (int j = 1; j <=i+1; ++j) {
+            cout<<j;
+
+        };
+        cout<<"\n";
     }
-    return 0;
 }
