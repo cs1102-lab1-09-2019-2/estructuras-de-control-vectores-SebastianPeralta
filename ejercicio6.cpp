edc35128f5ccd6be20492a500110060498ab890d
SebastianPeralta <54275767+SebastianPeralta@users.noreply.github.com>
2019-08-29 23:09:00 -0500

    Arreglado!

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -1,6 +1,13 @@
-/*
-
- Leer tres numeros del usuario separados por espacio: 10 15 20
- Y encuentra el mayor valor: 30
-
- */
+#include <iostream>
+using namespace std;
+int main() {
+    int n=3, j, max=0;
+    for (int i=0; i<n; i++){
+        cin>> j;
+        if (max<j||j==max) {
+            max = j;
+        }
+    }
+    cout<<max;
+    return 0;
+}
